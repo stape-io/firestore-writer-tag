31763d12929728d89a0ba28c1d6569b400b3c316
Denis Golubovskiy <bukashk0zzz@me.com>
2022-04-23 12:06:36 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Firestore Writer tag for Google Tag Manager Server Side
+
+TBD
+
+## Open Source
+
+Firestore Writer Tag for GTM Server Side is developed and maintained by [Stape Team](https://stape.io/) under the Apache 2.0 license.
---
+++ template.js
@@ -0,0 +1,51 @@
+﻿const JSON = require('JSON');
+const Firestore = require('Firestore');
+const getRequestHeader = require('getRequestHeader');
+const getAllEventData = require('getAllEventData');
+const getTimestampMillis = require('getTimestampMillis');
+const logToConsole = require('logToConsole');
+const getContainerVersion = require('getContainerVersion');
+const containerVersion = getContainerVersion();
+const isDebug = containerVersion.debugMode;
+const isLoggingEnabled = determinateIsLoggingEnabled();
+const traceId = getRequestHeader('trace-id');
+
+let input = data.addEventData ? getAllEventData() : {};
+
+let options = {merge: data.firebaseMerge ? true : false};
+if (data.firebaseProjectIdOverride) options.projectId = data.firebaseProjectId;
+
+if (data.addTimestamp) input[data.timestampFieldName] = getTimestampMillis();
+if (data.customDataList) data.customDataList.forEach(d => {input[d.name] = d.value;});
+
+Firestore.write(data.firebasePath, input, options).then((id) => {
+    if (isLoggingEnabled) {
+        logToConsole(JSON.stringify({
+            'Name': 'Firestore',
+            'Type': 'Message',
+            'TraceId': traceId,
+            'EventName': 'Write',
+            'DocumentId': id,
+            'DocumentInput': input,
+        }));
+    }
+
+    data.gtmOnSuccess();
+}, data.gtmOnFailure);
+
+
+function determinateIsLoggingEnabled() {
+    if (!data.logType) {
+        return isDebug;
+    }
+
+    if (data.logType === 'no') {
+        return false;
+    }
+
+    if (data.logType === 'debug') {
+        return isDebug;
+    }
+
+    return data.logType === 'always';
+}
